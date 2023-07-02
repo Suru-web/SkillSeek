ee703a602ec88244416863d7a89c424cd1a455d4
suraj <itsmesuru60@gmail.com>
2023-07-02 20:17:37 +0530

    added phone number and password checks successfully

--- app/src/main/java/com/helping/skillseek/MainActivity.java
+++ app/src/main/java/com/helping/skillseek/MainActivity.java
@@ -2,34 +2,71 @@ package com.helping.skillseek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
 
-    @SuppressLint("ObsoleteSdkInt")
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button contBtn;
+    TextInputLayout phno, pass;
+    String mobileNumber;
+    String Password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //This code sets the status bar color
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.white));
-            window.setNavigationBarColor(this.getResources().getColor(R.color.white));
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        window.setNavigationBarColor(this.getResources().getColor(R.color.white));
 
-            //This part of the code displays the statusbar icon color to black
-            int flags = window.getDecorView().getSystemUiVisibility();
-            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            window.getDecorView().setSystemUiVisibility(flags);
+        //This part of the code displays the statusbar icon color to black
+        int flags = window.getDecorView().getSystemUiVisibility();
+        flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        window.getDecorView().setSystemUiVisibility(flags);
+
+
+        contBtn = findViewById(R.id.continueButton);
+        phno = findViewById(R.id.phNumberInput);
+        pass = findViewById(R.id.passwordInput);
+
+        contBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+    if (view.getId() == R.id.continueButton){
+        mobileNumber = phno.getEditText().getText().toString();
+        Password = pass.getEditText().getText().toString();
+        if (mobileNumber.isEmpty()){
+            Toast toast = Toast.makeText(this,"Enter the Phone number",Toast.LENGTH_SHORT);
+            toast.show();
         }
-
+        else if (mobileNumber.length()<10){
+            Toast toast = Toast.makeText(this,"Enter 10 digits phone number properly",Toast.LENGTH_LONG);
+            toast.show();
+        } else if (Password.isEmpty()) {
+            Toast toast = Toast.makeText(this,"Enter Password",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else if (Password.length()<8){
+            Toast toast = Toast.makeText(this,"Minimum 8 characters password is required",Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else {
+            Intent intent = new Intent(this, loginPage.class);
+            startActivity(intent);
+        }
+    }
     }
 }
