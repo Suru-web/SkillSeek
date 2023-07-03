fbde6a112576f41efc5df2ce6352032d4609dd24
suraj <itsmesuru60@gmail.com>
2023-07-03 14:23:39 +0530

    * added login page with email and password input type

--- app/src/main/java/com/helping/skillseek/loginPage.java
+++ app/src/main/java/com/helping/skillseek/loginPage.java
@@ -3,6 +3,9 @@ package com.helping.skillseek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class loginPage extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class loginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        //This sets the background to white
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        window.setNavigationBarColor(this.getResources().getColor(R.color.white));
+
+        //This part of the code displays the statusbar icon color to black
+        int flags = window.getDecorView().getSystemUiVisibility();
+        flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        window.getDecorView().setSystemUiVisibility(flags);
     }
 }
