69279eb481b735865356989dce905417ead8c5eb
suraj <itsmesuru60@gmailc.com>
2023-07-11 18:58:48 +0530

    * Added otp verification * Added category select screen

--- app/src/main/java/com/helping/skillseek/CategorySelect.java
+++ app/src/main/java/com/helping/skillseek/CategorySelect.java
@@ -2,13 +2,19 @@ package com.helping.skillseek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.Vibrator;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class CategorySelect extends AppCompatActivity {
+public class CategorySelect extends AppCompatActivity implements View.OnClickListener {
 
+    ImageButton hirerBtn, workerBtn;
+    Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +23,30 @@ public class CategorySelect extends AppCompatActivity {
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.setStatusBarColor(this.getResources().getColor(R.color.white));
-        window.setNavigationBarColor(this.getResources().getColor(R.color.white));
+        window.setStatusBarColor(this.getResources().getColor(R.color.whiteDDDDD));
+        window.setNavigationBarColor(this.getResources().getColor(R.color.whiteDDDDD));
 
         //This part of the code displays the statusbar icon color to black
         int flags = window.getDecorView().getSystemUiVisibility();
         flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
         window.getDecorView().setSystemUiVisibility(flags);
+
+        hirerBtn = findViewById(R.id.imageButtonManager);
+        workerBtn = findViewById(R.id.imageButtonWorker);
+        hirerBtn.setOnClickListener(this);
+        workerBtn.setOnClickListener(this);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.imageButtonManager){
+            Toast.makeText(this, "You are a manager", Toast.LENGTH_SHORT).show();
+            vibrator.vibrate(5);
+
+        } else if (view.getId() == R.id.imageButtonWorker) {
+            Toast.makeText(this, "You are a worker", Toast.LENGTH_SHORT).show();
+            vibrator.vibrate(5);
+        }
     }
 }
