fcc8c34307e140a29987fdd05e059035a4fe4a55
suraj <itsmesuru60@gmailc.com>
2023-07-13 18:51:00 +0530

    * added few bits on hiree page

--- app/src/main/java/com/helping/skillseek/Hiree_main.java
+++ app/src/main/java/com/helping/skillseek/Hiree_main.java
@@ -3,6 +3,9 @@ package com.helping.skillseek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Hiree_main extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Hiree_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiree_main);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.bluePurp));
+        window.setNavigationBarColor(this.getResources().getColor(R.color.white));
+
+
     }
 }
