2a60a0b08abeef14524788a804a56890830a40e1
suraj <itsmesuru60@gmail.com>
2023-08-03 12:40:35 +0530

    * Added dropdown menu in hiree page

--- app/src/main/java/com/helping/skillseek/Hiree_main.java
+++ app/src/main/java/com/helping/skillseek/Hiree_main.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class Hiree_main extends AppCompatActivity {
 
+    String [] skills = {"Plumber","Carpainter","Painter","Garderner","House Cleaning"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterSkills;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +27,15 @@ public class Hiree_main extends AppCompatActivity {
         window.setStatusBarColor(this.getResources().getColor(R.color.bluePurp));
         window.setNavigationBarColor(this.getResources().getColor(R.color.white));
 
-
+        autoCompleteTextView = findViewById(R.id.dropDownAutoComplete);
+        adapterSkills = new ArrayAdapter<String>(this,R.layout.hiree_skill_dropdown,skills);
+        autoCompleteTextView.setAdapter(adapterSkills);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(Hiree_main.this,"Item "+item, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
