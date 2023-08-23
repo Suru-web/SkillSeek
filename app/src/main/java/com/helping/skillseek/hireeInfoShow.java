2a4eba9c3a70134cfff795422d20d9a134fd489f
Suraj <itsmesuru60@gmail.com>
2023-08-23 19:42:17 +0530

    * added ui elements in hireeInfoShow page

--- app/src/main/java/com/helping/skillseek/hireeInfoShow.java
+++ app/src/main/java/com/helping/skillseek/hireeInfoShow.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class hireeInfoShow extends AppCompatActivity {
@@ -12,6 +15,16 @@ public class hireeInfoShow extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiree_info_show);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        window.setNavigationBarColor(this.getResources().getColor(R.color.white));
+
+        int flags = window.getDecorView().getSystemUiVisibility();
+        flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        window.getDecorView().setSystemUiVisibility(flags);
 
 
         Intent intent = getIntent();
